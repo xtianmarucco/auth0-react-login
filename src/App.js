35a084888d3ea0a95bae8534d4d9cb7c65a472bb
xtianmarucco <christian.marucco@gmail.com>
2024-04-29 00:49:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./components/LoginButton";
+import Profile from "./components/Profile";
+import LogoutButton from "./components/LogOutButton";
 
 function App() {
+  const { isAuthenticated, user } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!isAuthenticated && (
+          <>
+            <h1>Welcome to My App!</h1>
+            <LoginButton />
+          </>
+        )}
+
+        {isAuthenticated && (
+          <>
+            <h1>Welcome, {user.name}!</h1>
+            <Profile />
+            <LogoutButton />
+          </>
+        )}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+<Auth0Provider
+    domain="dev-w72rsxyacf7i3w27.us.auth0.com"
+    clientId="2tfNHFbAZcA3hPMp7l2FjRCgfEM2RcRV"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
